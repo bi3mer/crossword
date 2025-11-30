41dbf0fdbf48d6e0f53411fde5ecbbfd13619e30
bi3mer <colan@comcast.net>
2025-11-30 12:23:21 -0500

    render to texture and the render to screen implemented

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include "clues.h"
@@ -5,22 +6,39 @@
 
 int main(void)
 {
-    const int screen_width = 800;
-    const int screen_height = 450;
+    const int texture_width = 1080;
+    const int texture_height = 720;
 
-    InitWindow(screen_width, screen_height, "Raylib Template");
+    InitWindow(texture_width, texture_height, "Crossword");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
+
+    RenderTexture2D target = LoadRenderTexture(texture_width, texture_height);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
+
+        // render to texture
+        BeginTextureMode(target);
         ClearBackground(RAYWHITE);
 
         DrawText(words[0].word, 190, 200, 20, BLACK);
 
+        EndTextureMode();
+
+        // render the texture
+        BeginDrawing();
+        const int W = GetScreenWidth();
+        const int H = GetScreenHeight();
+        DrawTexturePro(target.texture,
+                       (Rectangle){0, 0, (float)target.texture.width,
+                                   (float)-target.texture.height},
+                       (Rectangle){0, 0, W, H}, (Vector2){0, 0}, 0, WHITE);
+
         EndDrawing();
     }
 
+    UnloadRenderTexture(target);
     CloseWindow();
 
     return 0;
