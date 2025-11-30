4ec2eb71e1c52011325fc0bfae538b3a24ebe1c2
bi3mer <colan@comcast.net>
2025-11-30 13:27:23 -0500

    main.c now allows the player to move a camera around so that they can, in the future, see what is going on in the crossword. The movement isn't perfect, right now, though. I have this min and max thing I tried that I think i will get rid of in later iterations of the game.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "clues.h"
 #include "raylib.h"
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 int main(void)
 {
@@ -13,29 +18,73 @@ int main(void)
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
+    int min_x, max_x, min_y, max_y;
+    min_x = 0;
+    max_x = texture_width;
+    min_y = 0;
+    max_y = texture_height;
+
+    Camera2D camera = {0};
+    camera.zoom = 1.0f;
+
     RenderTexture2D target = LoadRenderTexture(texture_width, texture_height);
+    int selected_i = -1;
+    selected_i = 0;
 
     while (!WindowShouldClose())
     {
+        // handle mouse input
+        {
+            if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) ||
+                IsMouseButtonDown(MOUSE_MIDDLE_BUTTON) ||
+                IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
+            {
+                const Vector2 mouse_delta = GetMouseDelta();
+                const float new_x = camera.target.x - mouse_delta.x;
+                const float new_y = camera.target.y - mouse_delta.y;
 
-        // render to texture
-        BeginTextureMode(target);
-        ClearBackground(BLACK);
+                camera.target.x = MAX(MIN(new_x, max_x), min_x);
+                camera.target.y = MAX(MIN(new_y, max_y), min_y);
 
-        DrawText(words[0].word, 190, 200, 20, WHITE);
+                printf("%f, %f\n", new_y, camera.target.y);
+            }
+        }
 
-        EndTextureMode();
+        // handle keyboard input
+        {
+            const Word *selected = selected_i < 0 ? NULL : &words[selected_i];
+            int key = GetKeyPressed();
 
-        // render the texture
-        BeginDrawing();
-        const int W = GetScreenWidth();
-        const int H = GetScreenHeight();
-        DrawTexturePro(target.texture,
-                       (Rectangle){0, 0, (float)target.texture.width,
-                                   (float)-target.texture.height},
-                       (Rectangle){0, 0, W, H}, (Vector2){0, 0}, 0, WHITE);
+            while (selected != NULL && key != 0)
+            {
+                key = GetKeyPressed();
+            }
+        }
 
-        EndDrawing();
+        // render state to texture
+        {
+            BeginTextureMode(target);
+            BeginMode2D(camera);
+            ClearBackground(BLACK);
+
+            DrawText(words[0].word, 190, 200, 20, WHITE);
+
+            EndMode2D();
+            EndTextureMode();
+        }
+
+        // render the texture to the screen
+        {
+            BeginDrawing();
+            const int W = GetScreenWidth();
+            const int H = GetScreenHeight();
+            DrawTexturePro(target.texture,
+                           (Rectangle){0, 0, (float)target.texture.width,
+                                       (float)-target.texture.height},
+                           (Rectangle){0, 0, W, H}, (Vector2){0, 0}, 0, WHITE);
+
+            EndDrawing();
+        }
     }
 
     UnloadRenderTexture(target);
