23abc660d0db0705c81c1ac16b798b566042e7a2
Colan Biemer <bi3mer93@gmail.com>
2026-03-23 11:29:42 -0400

    fixed game_state init

--- src/game_state.c
+++ src/game_state.c
@@ -1,8 +1,11 @@
 #include "game_state.h"
 #include "const.h"
+#include "state_machine.h"
 
 void game_state_init(Game_State *state)
 {
+    state->current_state = STATE_MENU;
+
     state->camera.target.x = g_cell_width * CW_DIM / 2.f - 250;
     state->camera.target.y = g_cell_height * CW_DIM / 2.f - 250;
 }
